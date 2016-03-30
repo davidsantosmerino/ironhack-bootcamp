2f028f847dd51e98e859cafe59ed8b1015648503
davidsantosmerino <dsantosmerino92@gmail.com>
2016-03-31 00:33:44 +0200

    improve movie rating chart

--- week-1/graphical-movie-rating/graphical-movie-rating.rb
+++ week-1/graphical-movie-rating/graphical-movie-rating.rb
@@ -2,59 +2,92 @@ require "imdb"
 require "pry"
 
 class GraphicalMovieRating
+
 	def initialize filename
+		@counter = 0
 		@filename = filename
 		@movies = []
 	end
+
+	def start
+		read_file
+		draw
+	end
+
 	def read_file
 		titles = IO.readlines @filename
-		titles.each { |title| search_imdb title }
-		draw_chart
+		titles.each { |title| search_movie title }
 	end
-	def draw_chart
-		@movies.each { |movie| movie.draw_chart  }
+
+	def draw
+		print "\n"
+		draw_graphical_movie_rating
+		print "\n"
+		draw_graphical_movie_caption
 	end
-	def search_imdb title
+
+	def draw_graphical_movie_rating
+		charts = @movies.map { |movie| movie.chart  }
+		for i in 0..charts.first.size-1
+			charts.each_with_index do |chart, index|
+				print chart[i]
+			end
+			print "\n"
+		end
+	end
+
+	def draw_graphical_movie_caption
+		@movies.each { |movie| puts "#{movie.id}.#{movie.title}" }
+	end
+
+	def search_movie title
 		result_search = Imdb::Search.new title
-		movie = result_search.movies.first
-		details_by_id movie.id
+		first_result = result_search.movies.first
+		@movies << Movie.new(@counter, first_result.title, first_result.rating)
+		@counter += 1
 	end
-	def details_by_id movie_id
-		result_search = Imdb::Movie.new movie_id
-		@movies << Movie.new(result_search.title, result_search.rating)
-	end
+
 end
 
 class Movie
-	attr_reader :title, :rating
-	def initialize title, rating
+	attr_reader :id, :title, :rating
+
+	def initialize id, title, rating
+		@id = id
 		@title = title
 		@rating = rating
     @movie_chart = MovieChart.new self
 	end
-	def draw_chart
-		puts @movie_chart.rating_chart
+
+	def chart
+		@movie_chart.rating_chart
 	end
+
 end
 
 class MovieChart
+
 	def initialize movie
 		@movie = movie
 	end
+
 	def rating_chart
 		chart = []
-		chart << @movie.title
-		chart << "--"
+		chart << "|#{@movie.id}|"
+		chart << "---"
 		for i in 1..10
 			if @movie.rating && i <= @movie.rating
 				chart << "|#|"
+			elsif not @movie.rating
+				chart << "|?|"
 			else
 				chart << "| |"
 			end
 		end
 		chart.reverse
 	end
+
 end
 
 graphical_movie_rating = GraphicalMovieRating.new "movies.txt"
-graphical_movie_rating.read_file
+graphical_movie_rating.start
