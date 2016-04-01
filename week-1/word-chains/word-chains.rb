17c3f529d8c2bd430c5819e5e5b0d26766e61b55
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-01 09:25:41 +0200

    beautify style

--- week-1/word-chains/word-chains.rb
+++ week-1/word-chains/word-chains.rb
@@ -1,32 +1,28 @@
 require 'ruby-dictionary'
 
 class WordChain
+  def initialize(dictionary)
+    @dictionary = dictionary
+  end
 
-    def initialize(dictionary)
-        @dictionary = dictionary
+  def find_chain(first_word, second_word)
+    puts first_word
+    while second_word != first_word
+      i = 0
+      while first_word.split('')[i] != second_word.split('')[i] && i < first_word.size
+        temp_chars = first_word.split('')
+        temp_chars[i] = second_word.split('')[i]
+        if @dictionary.exists? temp_chars.join
+          puts temp_chars.join
+          first_word = temp_chars.join
+        end
+        i += 1
+      end
     end
-
-    def find_chain first_word, second_word
-	  	puts first_word
-	  	while second_word != first_word
-				success = false
-				i = 0
-				while (first_word.split('')[i] != second_word.split('')[i] && i < first_word.size)
-					temp_chars = first_word.split('')
-					temp_chars[i] = second_word.split('')[i]
-					if @dictionary.exists? temp_chars.join
-						puts temp_chars.join
-						first_word = temp_chars.join
-						success = true
-					end
-					i += 1
-				end
-			end
-		end
-
+  end
 end
 
-dictionary = Dictionary.from_file("/usr/share/dict/words")
+dictionary = Dictionary.from_file('/usr/share/dict/words')
 my_chain = WordChain.new(dictionary)
 
-my_chain.find_chain("cat", "dog")
+my_chain.find_chain('cat', 'dog')
