83f441832e111b4e0135d962f2e4f51d9ee64da3
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-05 10:04:16 +0200

    remove test folder

--- test/collections.rb
+++
@@ -1,9 +0,0 @@
-cities = ["miami", "barcelona", "madrid"]
-capitalized_cities = cities.map {|city| city.capitalize}
-
-puts capitalized_cities
-
-numbers = [2, 8, 6, 4]
-sum = numbers.reduce{|sum,x| sum + x}
-
-puts sum
