7d02f8000ba7fea858c46e048ec32ad5b917a1ca
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-19 12:25:49 +0200

    add project controller and index route

--- week-4/timetracking/config/routes.rb
+++ week-4/timetracking/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get "/" => "site#home"
 
+  get "/projects" => "projects#index"
+
   get "/contact" => "site#contact"
 
   get "/say_name/:name" => "site#say_name"
