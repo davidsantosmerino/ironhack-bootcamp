cb01ae8a380d1e4b9ee2de3f69113fb13ed25465
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-23 17:43:54 +0200

    add concert validations, passing specs

--- week-4/music-concert-network/app/models/concert.rb
+++ week-4/music-concert-network/app/models/concert.rb
@@ -1,2 +1,8 @@
 class Concert < ActiveRecord::Base
+  validates :artist, presence: true
+  validates :venue, presence: true
+  validates :city, presence: true
+  validates :date, presence: true
+  validates :price, presence: true, numericality: true
+  validates :description, presence: true
 end
