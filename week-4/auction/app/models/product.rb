e0b3662be371584f873c3e7a9d739ef1378598e6
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-23 16:32:22 +0200

    add some validations to pass the specs

--- week-4/auction/app/models/product.rb
+++ week-4/auction/app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :bids
+  validates :title, presence: true
+  validate :check_deadline
   validates :min_bid, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def expired
@@ -11,6 +13,17 @@ class Product < ActiveRecord::Base
     return nil unless self.bids.any?
 
     self.bids.order("amount DESC").first
+  end
+
+  private
+
+  def valid_deadline?
+    return false if self.deadline.nil?
+    self.deadline > DateTime.now
+  end
+
+  def check_deadline
+    errors.add(:deadline, "Deadline can't be before the current moment") unless valid_deadline?
   end
 
 end
