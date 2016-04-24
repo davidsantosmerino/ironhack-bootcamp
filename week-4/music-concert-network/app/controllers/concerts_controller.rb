afe90ddae3b25571b447a15b595cf6c096812c91
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-24 16:55:16 +0200

    add show and new concert

--- week-4/music-concert-network/app/controllers/concerts_controller.rb
+++ week-4/music-concert-network/app/controllers/concerts_controller.rb
@@ -2,4 +2,19 @@ class ConcertsController < ApplicationController
   def index
     @concerts = Concert.where("date >= ?", DateTime.now)
   end
+  def show
+    @concert = Concert.find_by(id: params[:id])
+  end
+  def new
+    @concert = Concert.new
+  end
+  def create
+    @concert = Concert.new(concert_params)
+    redirect_to root_path if @concert.save
+  end
+
+  private
+  def concert_params
+    params.require(:concert).permit(:artist, :venue, :city, :price, :date, :description, :asset)
+  end
 end
