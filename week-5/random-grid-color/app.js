d9bb3c453509a4ca6e0361ca8205d94c3effdcec
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-26 13:28:48 +0200

    refactor randomize pixel and add move pixel

--- week-5/random-grid-color/app.js
+++ week-5/random-grid-color/app.js
@@ -1,20 +1,29 @@
 $('document').ready(function(){
+  var COLORS = ['yellow', 'blue', 'green', 'red'];
+
   var gridPixels = 10*10;
-  var pixel = '<div class="pixel"></div>';
   for (var i = 0; i < gridPixels ; i++) {
+    var pixel = $('<div>').addClass('pixel');
     $('.main-container').append(pixel);
   }
 
+  function setCellColor(pixel) {
+    var randomColorIndex = Math.trunc (Math.random() * (COLORS.length));
+    pixel.attr('class', COLORS[randomColorIndex]);
+  }
+
   function randomizePixels() {
-    var colors = ['yellow', 'blue', 'green', 'red'];
     var allPixels = $('.main-container').children();
     allPixels.each(function(pixel){
-      var randomColorIndex = Math.trunc(Math.random() * (colors.length));
-      $(allPixels[pixel]).attr('class', colors[randomColorIndex])
+      setCellColor($(this));
     });
   }
 
+  $('.pixel').on('click', function(){
+    setCellColor($(this));
+  });
 
-  setInterval(randomizePixels, 1000);
+
+  //setInterval(randomizePixels, 1000);
 
 });
