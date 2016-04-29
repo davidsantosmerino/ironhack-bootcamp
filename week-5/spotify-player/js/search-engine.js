ed17f37f8abbbc5f1d6c93856290f4605da5dce5
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-30 00:09:45 +0200

    update styles

--- week-5/spotify-player/js/search-engine.js
+++ week-5/spotify-player/js/search-engine.js
@@ -1,11 +1,18 @@
 (function(){
   window.SpotifyApp = window.SpotifyApp || {};
 
-  var RESULTS_LIMIT = 14;
+  var RESULTS_LIMIT = 13;
+  var ELEMENT = {
+    form: 'form.search',
+    searchInput: '#search-input',
+    noResults: '.no-results'
+  }
 
   var SearchEngine = function(){
     var Player = window.SpotifyApp.Player;
     this.player = new Player();
+    this.form = $(ELEMENT.form);
+    this.searchInput = $(ELEMENT.searchInput);
   }
 
   SearchEngine.prototype.init = function(){
@@ -18,19 +25,30 @@
     var url = `https://api.spotify.com/v1/search?type=track&query=${query}&limit=${RESULTS_LIMIT}`;
     $.get(url)
       .done(function(response){
+        searchEngine.hideNoResults();
         searchEngine.player.setPlayer(response);
       })
       .fail(function(error){
+        this.showNoResults();
         console.error(error.responseJSON);
       });
   }
 
   SearchEngine.prototype.addListeners = function(){
-    $('form.search').on('submit', function (e) {
+    this.form.on('submit', function (e) {
       e.preventDefault();
-      var query = $('#search-input').val();
+      var query = this.searchInput.val();
+      this.searchInput.val('');
       this.fetchTracks(query);
     }.bind(this));
+  }
+
+  SearchEngine.prototype.showNoResults = function(){
+    $(ELEMENT.noResults).slideDown('down');
+  }
+
+  SearchEngine.prototype.hideNoResults = function(){
+    $(ELEMENT.noResults).slideUp('down');
   }
 
   window.SpotifyApp.SearchEngine = SearchEngine;
--- week-5/spotify-player/js/track.js
+++ week-5/spotify-player/js/track.js
@@ -2,7 +2,13 @@
   window.SpotifyApp = window.SpotifyApp || {};
   window.SpotifyApp.Entities = window.SpotifyApp.Entities || {};
 
-
+  var ELEMENT = {
+    trackWrapper: '.track-wrapper',
+    title: '.js-title',
+    artist: '.js-artist',
+    cover: '.js-cover',
+    player: '.js-player'
+  }
   var MISSING_COVER_URL = 'img/microphone.png';
 
   var Track = function(track){
@@ -10,6 +16,10 @@
     this.artist = parseArtistsNames(track.artists);
     this.cover = parseCoverUrl(track.album.images);
     this.audio = track.preview_url;
+  }
+
+  Track.prototype.render = function(){
+    $(ELEMENT.trackWrapper).slideUp('slow', renderTransition.bind(this));
   }
 
   function parseArtistsNames(artists){
@@ -25,13 +35,13 @@
     return images.length > 0 ? images[0].url : MISSING_COVER_URL;
   }
 
-  Track.prototype.render = function(){
-    var trackWrapper = $('.track-wrapper');
-    $('.js-title').text(this.title);
-    $('.js-artist').text(this.artist);
-    $('.js-cover').attr('src',this.cover);
-    $('.js-player').attr('src',this.audio);
-    trackWrapper.fadeIn();
+  function renderTransition(){
+    var trackManager = this;
+    $(ELEMENT.title).text(trackManager.title);
+    $(ELEMENT.artist).text(trackManager.artist);
+    $(ELEMENT.cover).attr('src',trackManager.cover);
+    $(ELEMENT.player).attr('src',trackManager.audio);
+    $(ELEMENT.trackWrapper).slideDown('slow');
   }
 
   window.SpotifyApp.Entities.Track = Track;
