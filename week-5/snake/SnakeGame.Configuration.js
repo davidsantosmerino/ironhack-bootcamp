75e3dec902dee7cc54dd9994b087a27077410103
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-30 18:47:55 +0200

    remove unused vars

--- week-5/snake/SnakeGame.Configuration.js
+++ week-5/snake/SnakeGame.Configuration.js
@@ -1,9 +1,5 @@
 (function(){
   window.SnakeGame = window.SnakeGame || {};
-  var Game = window.SnakeGame.Game;
-  var Entities = window.SnakeGame.Entities;
-  var Cell = Entities.Cell;
-  var Constants = window.SnakeGame.Constants;
 
   var Configuration = function(rowSize, columnSize) {
     this.difficultyLevels = [250, 200, 150, 100, 80, 60, 40, 35]
