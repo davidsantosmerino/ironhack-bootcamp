3ffe3f838b1382c7c70ab1959603f29cb5405834
davidsantosmerino <dsantosmerino92@gmail.com>
2016-05-02 13:05:28 +0200

    add show ingredient

--- week-6/Sandwichr/app/controllers/api/ingredients_controller.rb
+++ week-6/Sandwichr/app/controllers/api/ingredients_controller.rb
@@ -1,7 +1,22 @@
 class Api::IngredientsController < ApplicationController
-
+  before_action :ensure_ingredient, only:[:show]
   def index
     ingredients = Ingredient.all
     render json: ingredients
+  end
+  def show
+    render json: @ingredient
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :calories, :amount)
+  end
+  def ensure_ingredient
+    @ingredient = Ingredient.find_by(id: params[:id])
+    render(
+      json: { errors: "Ingredient with id #{params[:id]} not found" },
+      status: 404
+    ) unless @ingredient
   end
 end
