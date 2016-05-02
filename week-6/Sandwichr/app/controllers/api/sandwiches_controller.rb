f8b0cc60bda313d2e11908eb3b93fca922d5e654
davidsantosmerino <dsantosmerino92@gmail.com>
2016-05-02 12:36:09 +0200

    add before actions ro avoid repeat conditionals

--- week-6/Sandwichr/app/controllers/api/sandwiches_controller.rb
+++ week-6/Sandwichr/app/controllers/api/sandwiches_controller.rb
@@ -1,31 +1,33 @@
 class Api::SandwichesController < ApplicationController
+  before_action :ensure_sandwich, only: [:show, :update]
+
   def index
     sandwiches = Sandwich.all
     render json: sandwiches
   end
   def show
-    sandwich = Sandwich.find(params[:id])
-    render json: sandwich
+    render json: @sandwich
   end
   def create
     sandwich = Sandwich.new(sandwich_params)
     if sandwich.save
       render json: sandwich, status: :created
     else
-      render json: { errors: sandwich.errors.full_messages }, status: 422
     end
   end
   def update
-    sandwich = Sandwich.find(params[:id])
-    if sandwich.update(sandwich_params)
-      render json: sandwich, status: :ok
+    if @sandwich.update(sandwich_params)
+      render json: @sandwich, status: :ok
     else
-      render json: { errors: sandwich.errors.full_messages }, status: 422
+      render json: { errors: @sandwich.errors.full_messages }, status: 422
     end
   end
-
   private
   def sandwich_params
     params.require(:sandwich).permit(:name, :bread_type)
+  end
+  def ensure_sandwich
+    @sandwich = Sandwich.find(params[:id])
+    render json: { errors: sandwich.errors.full_messages }, status: 422 unless @sandwich
   end
 end
