f35321bcb99b1acb891d84e0a180c0cbc150040d
davidsantosmerino <dsantosmerino92@gmail.com>
2016-05-03 17:04:49 +0200

    add more seeds

--- week-6/Sandwichr/db/seeds.rb
+++ week-6/Sandwichr/db/seeds.rb
@@ -1,3 +1,5 @@
 bacon_ingredient = Ingredient.create(name: "bacon", calories: 250, amount: 0.5)
+tomato_ingredient = Ingredient.create(name: "tomato", calories: 60, amount: 0.3)
+lettuce_ingredient = Ingredient.create(name: "lettuce", calories: 30, amount: 0.3)
 bacon_sandwich = Sandwich.create(name: "Bacon sandwich", bread_type: "regular")
 bacon_sandwich.ingredients.push(bacon_ingredient)
